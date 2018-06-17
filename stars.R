4aca7f6d267ea175841c0ed13a7d6da521096aa3
Tim Sneath <timsneath@google.com>
2018-06-16 17:44:50 -0700

    Updated chart

--- stars.R
+++ stars.R
@@ -3,27 +3,36 @@ library(dplyr)
 library(ggplot2)
 library(ggthemes)
 
+# Clear plot window
+while (dev.cur()>1) dev.off()
+
 # This is the key for the relevant Google Sheets
 starSheet <- gs_key("1s0Eyl4VLgc0dFrBsJFnrq8o_ghwFk6OchLI7GRsKfuo")
 
 # Load and cleanse the Google Sheets
-flutterStars <- starSheet %>% gs_read(ws="Flutter", range="A1:B16")
-flutterStars$Date <- as.Date(flutterStars$Date, format="%m/%d/%Y")
-colnames(flutterStars) <- c("Date", "Stars")
+flutter <- starSheet %>% gs_read(ws = "Flutter", range = "A1:B20")
+flutter$Date <- as.Date(flutter$Date, format = "%m/%d/%Y")
+flutter$project <- "Flutter"
+colnames(flutter) <- c("date", "stars", "project")
 
-angularStars <- starSheet %>% gs_read(ws="Angular", range="A1:B16")
-angularStars$Date <- as.Date(angularStars$Date, format="%m/%d/%Y")
-colnames(angularStars) <- c("Date", "Stars")
+angular <- starSheet %>% gs_read(ws = "Angular", range = "A1:B16")
+angular$Date <- as.Date(angular$Date, format = "%m/%d/%Y")
+angular$project <- "Angular"
+colnames(angular) <- c("date", "stars", "project")
 
-goStars <- starSheet %>% gs_read(ws="Go", range="A1:B16")
-goStars$Date <- as.Date(goStars$Date, format="%m/%d/%Y")
-colnames(goStars) <- c("Date", "Stars")
+go <- starSheet %>% gs_read(ws = "Go", range = "A1:B17")
+go$Date <- as.Date(go$Date, format = "%m/%d/%Y")
+go$project <- "Go"
+colnames(go) <-c("date", "stars", "project")
+
+repos <- rbind(flutter, angular, go)
 
 # Now plot
-graph <- ggplot(flutterStars, aes(Date, Stars)) +
-  geom_line() +
-  geom_line(data=angularStars) +
-  geom_line(data=goStars) +
-  ggtitle("GitHub Stars by Date") + 
-  theme_fivethirtyeight()
-graph
+ggplot(repos, aes(date, stars)) +
+  geom_line(aes(color = project)) +
+  labs(title = "GitHub stars by date",
+       caption = "Source: http://timqian.com/star-history/") + 
+  theme_fivethirtyeight() + 
+  scale_color_fivethirtyeight() +
+  theme(legend.position = "top", 
+        legend.title = element_blank())
