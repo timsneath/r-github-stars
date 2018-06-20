68478e79f5219c0a2445ef430b17d11b887e6a2d
Tim Sneath <timsneath@google.com>
2018-06-19 21:03:01 -0700

    Add Flutter-only chart

--- flutterStars.R
+++ flutterStars.R
@@ -17,33 +17,19 @@ flutter$Date <- as.Date(flutter$Date, format = "%m/%d/%Y")
 flutter$project <- "Flutter"
 colnames(flutter) <- c("date", "stars", "project")
 
-angular <- starSheet %>% gs_read(ws = "Angular", range = "A1:B20")
-angular$Date <- as.Date(angular$Date, format = "%m/%d/%Y")
-angular$project <- "Angular"
-colnames(angular) <- c("date", "stars", "project")
-
-go <- starSheet %>% gs_read(ws = "Go", range = "A1:B20")
-go$Date <- as.Date(go$Date, format = "%m/%d/%Y")
-go$project <- "Go"
-colnames(go) <-c("date", "stars", "project")
-
-repos <- rbind(flutter, angular, go)
-
 # Now plot
-ggplot(repos, aes(date, stars)) +
+ggplot(flutter, aes(date, stars)) +
   geom_line(aes(color = project)) +
   labs(title = "GitHub stars by date",
        caption = "Source: http://timqian.com/star-history/") + 
-  scale_color_discrete(breaks=c("Flutter", "Angular", "Go")) + 
+  scale_fill_fivethirtyeight() + 
   scale_x_date(date_breaks = "3 months",
-    date_minor_breaks = "1 month",
-    labels = date_format("%b %y")) + 
+               date_minor_breaks = "1 month",
+               labels = date_format("%b %y")) + 
   scale_y_continuous(label = comma) +
   coord_cartesian(xlim = c(as.Date("2016/01/01"), as.Date("2018/06/17"))) + 
   theme_fivethirtyeight() + 
-  theme(legend.position = "bottom", 
-        legend.justification = "right",
-        legend.title = element_blank(),
+  theme(legend.position = "none", 
         plot.title = element_text(margin=margin(0,0,20,0)),
-        plot.caption = element_text(size=9),
+        plot.caption = element_text(size=9, margin=margin(20,0,0,0)),
         text=element_text(family="Open Sans"))
