f1672fec553379397f8d2d2f9ea695e278ba1194
Tim Sneath <timsneath@google.com>
2018-06-27 16:06:03 -0700

    update for moving date window

--- stars.R
+++ stars.R
@@ -13,17 +13,17 @@ while (dev.cur() > 1)
 star_sheet <- gs_key("1s0Eyl4VLgc0dFrBsJFnrq8o_ghwFk6OchLI7GRsKfuo")
 
 # Load and cleanse the Google Sheets
-flutter <- star_sheet %>% gs_read(ws = "Flutter", range = "A1:B20")
+flutter <- star_sheet %>% gs_read(ws = "Flutter", range = "A1:B200")
 flutter$Date <- as.Date(flutter$Date, format = "%m/%d/%Y")
 flutter$project <- "Flutter"
 colnames(flutter) <- c("date", "stars", "project")
 
-angular <- star_sheet %>% gs_read(ws = "Angular", range = "A1:B20")
+angular <- star_sheet %>% gs_read(ws = "Angular", range = "A1:B200")
 angular$Date <- as.Date(angular$Date, format = "%m/%d/%Y")
 angular$project <- "Angular"
 colnames(angular) <- c("date", "stars", "project")
 
-go <- star_sheet %>% gs_read(ws = "Go", range = "A1:B20")
+go <- star_sheet %>% gs_read(ws = "Go", range = "A1:B200")
 go$Date <- as.Date(go$Date, format = "%m/%d/%Y")
 go$project <- "Go"
 colnames(go) <- c("date", "stars", "project")
@@ -42,13 +42,14 @@ ggplot(repos, aes(date, stars)) +
     labels = date_format("%b %y")
   ) +
   scale_y_continuous(label = comma) +
-  coord_cartesian(xlim = c(as.Date("2016/01/01"), as.Date("2018/06/17"))) +
+  coord_cartesian(xlim = c(as.Date("2016/01/01"), Sys.Date())) +
   theme_fivethirtyeight() +
   theme(
     legend.position = "bottom",
     legend.justification = "right",
     legend.title = element_blank(),
     plot.title = element_text(margin = margin(0, 0, 20, 0)),
+    plot.background = element_rect(fill = "white"),
     plot.caption = element_text(size = 9),
     text = element_text(family = "Open Sans")
   )
